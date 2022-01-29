edd885f208e031a4b6875ed77729c35ca69a54fd
owkmear <h3s8qp9xw3f@gmail.com>
2022-01-30 00:08:40 +0300

    Добавлено logo. Оформлен начальный экран

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Image, Text } from "react-native";
 import { View } from "../components/Themed";
 import { HomeProps } from "../types";
 
 export default function HomeScreen({ navigation }: HomeProps) {
   return (
     <View style={styles.container}>
+      <View>
+        <Text
+          style={{
+            textAlign: "right",
+            fontSize: 34,
+            fontWeight: "bold",
+            letterSpacing: 2,
+            color: "#686868",
+          }}
+        >
+          JavaScript
+        </Text>
+        <Text
+          style={{
+            textAlign: "right",
+            fontSize: 20,
+            fontStyle: "italic",
+            color: "#a5a5a5",
+            lineHeight: 17,
+          }}
+        >
+          тестирование
+        </Text>
+      </View>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: 15,
+        }}
+      >
+        <Image
+          style={styles.logo}
+          source={require("../assets/images/logo.png")}
+        />
+      </View>
       <View style={styles.start}>
         <Button
           title="Начать"
@@ -30,7 +66,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  logo: {
+    width: 200,
+    height: 200,
+  },
   start: {
-    marginBottom: 15,
+    paddingTop: 50,
+    paddingBottom: 20,
   },
 });

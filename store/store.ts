c4fc84624ddb36f9b2f54b19152fefc4050b3025
owkmear <h3s8qp9xw3f@gmail.com>
2022-03-29 18:53:12 +0300

    Fix serialization error with redux-persist and redux-toolkit

--- store/store.ts
+++ store/store.ts
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import { combineReducers } from "redux";
 import logger from "redux-logger";
 import questionsReducer from "./questionsSlice";
@@ -18,7 +26,12 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;

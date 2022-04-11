11841e816937b83c74d0ebe785e4ffdc226ffdba
owkmear <h3s8qp9xw3f@gmail.com>
2022-04-11 11:21:25 +0300

    Added 1 question "BASICS" theme

--- store/questions/BASICS.ts
+++ store/questions/BASICS.ts
@@ -14,6 +14,21 @@ console.log(result);`,
     variants: ["true", "6", "false", "3"],
     explanation: `В данном случае мы проверяем, есть ли в массиве 'array' элемент с индексом '13'`,
   },
+  2: {
+    id: "28dc98de-868b-4109-9c38-074045473a87",
+    grade: Grades.Junior,
+    theme: "Типы данных",
+    question: `Что будет выведено в консоль?`,
+    code: `const fruits = [];
+fruits[0] = "apple";
+fruits[1] = "pear";
+fruits[5] = "banana";
+
+console.log(fruits.length);`,
+    correctAnswer: 2,
+    variants: [`0`, `6`, `5`, `3`],
+    explanation: `Отсутствующий промежуток массива будет заполняться значениями 'undefined', а в консоли массив будет выглядеть следующим образом: "['apple', 'pear', empty × 3, 'banana']"`,
+  },
 };
 
 export default questions;

a488014ad1bdc6cf29cb578e0499d110f438c508
owkmear <h3s8qp9xw3f@gmail.com>
2022-04-28 23:57:50 +0300

    Fix working on android

--- i18n.ts
+++ i18n.ts
@@ -1,45 +1,42 @@
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      "en-US": {
-        translations: {
-          settings: "Settings",
-          back: "Back",
-          testing: "testing",
-          start: "Start",
-          page_not_found: "Page not found",
-          back_to_menu: "Back to menu",
-          to_answer: "Answer",
-          grade: 'Grade: "{{grade}}"',
-        },
-      },
-      "ru-RU": {
-        translations: {
-          settings: "Настройки",
-          back: "Назад",
-          testing: "тестирование",
-          start: "Начать",
-          page_not_found: "Страница не найдена",
-          back_to_menu: "Вернуться в меню",
-          to_answer: "Ответить",
-          grade: 'Уровень: "{{grade}}"',
-        },
+i18n.use(initReactI18next).init({
+  resources: {
+    "en-US": {
+      translations: {
+        settings: "Settings",
+        back: "Back",
+        testing: "testing",
+        start: "Start",
+        page_not_found: "Page not found",
+        back_to_menu: "Back to menu",
+        to_answer: "Answer",
+        grade: 'Grade: "{{grade}}"',
       },
     },
-    fallbackLng: "en-US",
-    debug: true,
-    ns: ["translations"],
-    defaultNS: "translations",
-    keySeparator: false,
-    interpolation: {
-      escapeValue: false,
+    "ru-RU": {
+      translations: {
+        settings: "Настройки",
+        back: "Назад",
+        testing: "тестирование",
+        start: "Начать",
+        page_not_found: "Страница не найдена",
+        back_to_menu: "Вернуться в меню",
+        to_answer: "Ответить",
+        grade: 'Уровень: "{{grade}}"',
+      },
     },
-  });
+  },
+  compatibilityJSON: "v3",
+  fallbackLng: "en-US",
+  debug: true,
+  ns: ["translations"],
+  defaultNS: "translations",
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;

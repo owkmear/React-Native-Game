fd2ea988c4032afabde24f582775e5fab4bfd710
owkmear <h3s8qp9xw3f@gmail.com>
2022-05-20 16:07:23 +0300

    Added FR language

--- i18n.ts
+++ i18n.ts
@@ -4,6 +4,7 @@ import translation_BS from "./locales/bs-BS.json";
 import translation_DE from "./locales/de-DE.json";
 import translation_US from "./locales/en-US.json";
 import translation_ES from "./locales/es-ES.json";
+import translation_FR from "./locales/fr-FR.json";
 import translation_RU from "./locales/ru-RU.json";
 import translation_TW from "./locales/zh-TW.json";
 
@@ -13,6 +14,7 @@ i18n.use(initReactI18next).init({
     "de-DE": { translation: translation_DE },
     "en-US": { translation: translation_US },
     "es-ES": { translation: translation_ES },
+    "fr-FR": { translation: translation_FR },
     "ru-RU": { translation: translation_RU },
     "zh-TW": { translation: translation_TW },
   },

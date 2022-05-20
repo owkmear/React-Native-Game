3aae4cd1b27c9b1ff818926e208b06460790d08a
owkmear <h3s8qp9xw3f@gmail.com>
2022-05-20 15:27:35 +0300

    Added Spain language

--- i18n.ts
+++ i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import translation_BS from "./locales/bs-BS.json";
 import translation_DE from "./locales/de-DE.json";
 import translation_US from "./locales/en-US.json";
+import translation_ES from "./locales/es-ES.json";
 import translation_RU from "./locales/ru-RU.json";
 import translation_TW from "./locales/zh-TW.json";
 
@@ -11,6 +12,7 @@ i18n.use(initReactI18next).init({
     "bs-BS": { translation: translation_BS },
     "de-DE": { translation: translation_DE },
     "en-US": { translation: translation_US },
+    "es-ES": { translation: translation_ES },
     "ru-RU": { translation: translation_RU },
     "zh-TW": { translation: translation_TW },
   },
